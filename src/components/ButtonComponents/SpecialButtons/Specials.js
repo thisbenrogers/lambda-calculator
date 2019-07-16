5d669ff13f1278909cb5f72862bb8e4a21b93e72
BenR <20133902+thisbenrogers@users.noreply.github.com>
2019-07-16 12:36:39 -0500

    :sparkles: Adds data to state
    Steps 1 & 2, Imports/Exports data from data.js, adds dataa to state.

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -3,9 +3,12 @@ import React from "react";
 //import any components needed
 
 //Import your array data to from the provided data file
+import { specials } from '../../../data';
+
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialState, setSpecialState] = useState(specials);
 
   return (
     <div>
